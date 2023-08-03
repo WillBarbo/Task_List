3bde3936e4a04f68a1abb10527d89715c849de41
Willian Barbosa <willianbsilva96@gmail.com>
2023-08-03 16:46:27 -0300

    task_list

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,6 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'react/jsx-filename-extension'
+    'react/jsx-filename-extension': 0,
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return <h1>Hello World!</h1>;
+export default function App() {
+  return <p>Lorem ipsilum</p>;
 }
-
-export default App;
